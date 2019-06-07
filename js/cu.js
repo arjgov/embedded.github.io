abc3be2439c23cbca656abe00455ede6c71d39ea
Arjun Govindan <36874143+arjgov@users.noreply.github.com>
2019-06-08 00:42:08 +0530

    Add files via upload

---
+++ js/cu.js
@@ -0,0 +1,3 @@
+   $(window).load(function(){
+    $('#preloader').fadeOut('delay=55 s',function(){$(this).remove();});
+  });
---
+++ js/custom.js
@@ -0,0 +1,94 @@
+
+        $(window).load(function(){
+    $('#preloader').fadeOut('slow',function(){$(this).remove();});
+  });
+
+
+// Flexslider
+$(function(){
+  /* FlexSlider */
+  $('.flexslider').flexslider({
+      animation: "fade",
+      directionNav: false
+  });
+
+  new WOW().init();
+
+  // Magnific Pop up for Portfolio section
+  $('.portfolio-container').magnificPopup({
+    delegate: '.portfolio-popup', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery: {
+      enabled: true
+    }    
+  });
+
+  
+});
+
+// isotope
+jQuery(document).ready(function($){
+
+  if ( $('.iso-box-wrapper').length > 0 ) { 
+
+      var $container  = $('.iso-box-wrapper'), 
+        $imgs     = $('.iso-box img');
+
+      $container.imagesLoaded(function () {
+
+        $container.isotope({
+        layoutMode: 'fitRows',
+        itemSelector: '.iso-box'
+        });
+
+        $imgs.load(function(){
+          $container.isotope('reLayout');
+        })
+
+      });
+
+      //filter items on button click
+      $('.filter-wrapper li a').click(function(){
+
+          var $this = $(this), filterValue = $this.attr('data-filter');
+
+      $container.isotope({ 
+        filter: filterValue,
+        animationOptions: { 
+            duration: 750, 
+            easing: 'linear', 
+            queue: false, 
+        }                
+      });             
+
+      // don't proceed if already selected 
+      if ( $this.hasClass('selected') ) { 
+        return false; 
+      }
+
+      var filter_wrapper = $this.closest('.filter-wrapper');
+      filter_wrapper.find('.selected').removeClass('selected');
+      $this.addClass('selected');
+
+        return false;
+      }); 
+
+  }
+
+});
+
+// Hide mobile menu after clicking on a link
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('.navbar-default a, a,').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 68
+            }, 1000);
+            event.preventDefault();
+        });
+    });
